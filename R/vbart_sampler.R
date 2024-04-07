05e2ccb51940a16f38255e07a4980b10c1e5da4e
juanyjuan4 <jdwwhetten2@gmail.com>
2024-04-06 19:41:25 -0600

    Output is now serialized as json, which is a lot faster to convert to R data types

--- R/vbart_sampler.R
+++ R/vbart_sampler.R
@@ -14,44 +14,6 @@
 #' @examples
 #' vbart_sampler(y, 1000, 50, 2, lon, lat)
 vbart_sampler <- function(y, n_samples, n_trees, delta, lon, lat) {
+  .Call(.vbart_sampler, y, n_samples, n_trees, delta, lon, lat)
 
-  expected_max_b <- delta * 6
-  nrows = length(y)
-
-  s_list <- vector(mode = "list", length = n_samples)
-  mu_list <- vector(mode = "list", length = n_samples)
-
-  for (scan_index in 1:n_samples) {
-    s_list[[scan_index]] <- vector(mode = "list", length = n_trees)
-    mu_list[[scan_index]] <- vector(mode = "list", length = n_trees)
-    for (tree_index in 1:n_trees) {
-      s_list[[scan_index]][[tree_index]] <- list(
-        "s_mat" = matrix(rep(-99999, 2 * expected_max_b), ncol = 2),
-        "dtr" = rep(-1, expected_max_b),
-        "dtd" = rep(-1, expected_max_b),
-        "mu_index" = rep(-1L, nrows),
-        "log_crit" = 0.0
-      )
-      mu_list[[scan_index]][[tree_index]] <- rep(-1, expected_max_b);
-    }
-  }
-  tau_vec <- rep(-1, n_samples);
-  yhat_mat <- matrix(rep(-1, nrows * n_samples), ncol = nrows)
-  
-  .Call(.vbart_sampler, y, n_samples, n_trees, delta, lon, lat, s_list, mu_list, tau_vec, yhat_mat)
-  
-  for (si in 1:n_samples) {
-    for (ti in 1:n_trees) {
-      temp_s <- s_list[[si]][[ti]]$s_mat
-      s_list[[si]][[ti]]$s_mat <- temp_s[temp_s[,1]!=-99999,temp_s[1,]!=-99999]
-      temp_dtr <- s_list[[si]][[ti]]$dtr
-      s_list[[si]][[ti]]$dtr <- temp_dtr[temp_dtr!=-1]
-      temp_dtd <- s_list[[si]][[ti]]$dtd
-      s_list[[si]][[ti]]$dtd <- temp_dtd[temp_dtd!=-1]
-      temp_mu <- mu_list[[si]][[ti]]
-      mu_list[[si]][[ti]] <- temp_mu[temp_mu!=-1]
-    }
-  }
-  
-  list("s_list" = s_list, "mu_list" = mu_list, "tau_vec" = tau_vec, "yhat_mat" = yhat_mat)
 }
--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -8,15 +8,24 @@ use geo::Point;
 use rand::prelude::*;
 use rand_distr::{Distribution, Gamma, Normal, Poisson, Standard, Uniform};
 use roxido::*;
+use serde::Serialize;
 use statrs::function::gamma::gamma;
 
-#[derive(Clone)]
+#[derive(Clone, Serialize)]
 struct Sobj {
     s_mat: Vec<Vec<f64>>,
     dtr: Vec<f64>,
     dtd: Vec<f64>,
     mu_index: Vec<i32>,
     log_crit: f64,
+}
+
+#[derive(Clone, Serialize)]
+struct Retval {
+    s_list: Vec<Vec<Sobj>>,
+    mu_list: Vec<Vec<Vec<f64>>>,
+    tau_vec: Vec<f64>,
+    yhat_mat: Vec<Vec<f64>>,
 }
 
 #[roxido]
@@ -27,10 +36,6 @@ fn vbart_sampler(
     delta: f64,
     lon: &RObject<RVector, f64>,
     lat: &RObject<RVector, f64>,
-    s_list_r: &mut RObject<RList>,
-    mu_list_r: &mut RObject<RList>,
-    tau_vec_r: &mut RObject<RVector, f64>,
-    yhat_mat_r: &mut RObject<RMatrix, f64>,
 ) {
     let mut rng = rand::thread_rng();
     let nrows = y.len();
@@ -124,111 +129,16 @@ fn vbart_sampler(
         }
         tau_vec[scan_index] = sample_tau_given_everything(&yhat, &dy, 2.0, 2.0);
     }
-    rprintln!("Finished sampling. Writing to list now");
+    rprintln!("Finished sampling. Writing to json now.");
 
-    // This is where I'm going to populate all of the R data objects that were passed in.
-    for scan_index in 0..n_samples {
-        for tree_index in 0..n_trees {
-            // s_list
-            let temp_init = s_list_r
-                .get_mut(scan_index)
-                .stop()
-                .as_list_mut()
-                .stop()
-                .get_mut(tree_index)
-                .stop()
-                .as_list_mut()
-                .stop();
+    let retval = Retval {
+        s_list,
+        mu_list,
+        tau_vec,
+        yhat_mat,
+    };
 
-            let temp: &mut RObject<RMatrix, f64> = temp_init
-                .get_mut(0)
-                .stop()
-                .as_matrix_mut()
-                .stop()
-                .as_f64_mut()
-                .stop();
-            for i in 0..s_list[scan_index][tree_index].s_mat.len() {
-                for j in 0..2 {
-                    temp.set((i, j), s_list[scan_index][tree_index].s_mat[i][j])
-                        .stop();
-                }
-            }
-
-            let temp: &mut RObject<RVector, f64> = temp_init
-                .get_mut(1)
-                .stop()
-                .as_vector_mut()
-                .stop()
-                .as_f64_mut()
-                .stop();
-            for i in 0..s_list[scan_index][tree_index].dtr.len() {
-                temp.set(i, s_list[scan_index][tree_index].dtr[i]).stop()
-            }
-
-            let temp: &mut RObject<RVector, f64> = temp_init
-                .get_mut(2)
-                .stop()
-                .as_vector_mut()
-                .stop()
-                .as_f64_mut()
-                .stop();
-            for i in 0..s_list[scan_index][tree_index].dtd.len() {
-                temp.set(i, s_list[scan_index][tree_index].dtd[i]).stop()
-            }
-
-            let temp: &mut RObject<RVector, i32> = temp_init
-                .get_mut(3)
-                .stop()
-                .as_vector_mut()
-                .stop()
-                .as_i32_mut()
-                .stop();
-            for i in 0..s_list[scan_index][tree_index].mu_index.len() {
-                temp.set(i, s_list[scan_index][tree_index].mu_index[i])
-                    .stop()
-            }
-
-            let temp: &mut RObject<RScalar, f64> = temp_init
-                .get_mut(4)
-                .stop()
-                .as_scalar_mut()
-                .stop()
-                .as_f64_mut()
-                .stop();
-            temp.set(s_list[scan_index][tree_index].log_crit);
-
-            // mu_list
-            let temp: &mut RObject<RVector, f64> = mu_list_r
-                .get_mut(scan_index)
-                .stop()
-                .as_list_mut()
-                .stop()
-                .get_mut(tree_index)
-                .stop()
-                .as_vector_mut()
-                .stop()
-                .as_f64_mut()
-                .stop();
-            for i in 0..mu_list[scan_index][tree_index].len() {
-                temp.set(i, mu_list[scan_index][tree_index][i]).stop();
-            }
-
-            // tau_vec
-            for i in 0..tau_vec.len() {
-                tau_vec_r.set(i, tau_vec[i]).stop();
-            }
-
-            // yhat_mat
-            for i in 0..yhat_mat.len() {
-                for j in 0..yhat_mat[i].len() {
-                    yhat_mat_r.set((i, j), yhat_mat[i][j]).stop();
-                }
-            }
-        }
-    }
-
-    let ret = RObject::<RScalar, i32>::from_value(1, pc);
-    ret
+    serde_json::to_string(&retval).unwrap().as_str().to_r(pc)
 }
 
 fn sample_s_given_r_tau<'a>(
@@ -249,7 +159,6 @@ fn sample_s_given_r_tau<'a>(
         s_mat[i][0] = uno.sample(&mut rng);
         s_mat[i][1] = dos.sample(&mut rng);
     }
-    // rprintln!("{:?}", s_mat);
 
     let mut d_mat = vec![vec![0.0; b + 1]; nrow];
     for i in 0..nrow {
@@ -298,12 +207,7 @@ fn sample_s_given_r_tau<'a>(
         .map(|(x, y)| (-(1.0 + tau * y).ln() + tau.powi(2) / (1.0 + tau * y) * x.powi(2)) as f64)
         .sum();
     let log_crit: f64 = log_crit_temp / 2.0 + pois_pmf(b as i32, delta).ln();
-    // let mut s_new = vec![vec![0.0; 2]; b + 1];
-    // for i in 0..(b + 1) {
-    //     for j in 0..2 {
-    //         s_new[i][j] = s_mat[i][j];
-    //     }
-    // }
+
     let ret = Sobj {
         s_mat,
         dtr,
